f627627225259d666862320524bae46b05681bcf
Darshan K <darshankulkarni34@gmail.com>
2026-05-12 10:01:39 +0530

    feaat: Added redirect for / to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Projects from "./pages/Projects";
 import ProjectDetails from "./pages/ProjectDetails";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+
 function App() {
   return (
     <Routes>
-     
+     // add this route
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
         <Route
